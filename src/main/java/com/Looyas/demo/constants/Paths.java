aeeef50322eed1942f03dbfe141c9ed8016ce721
Nour Mhamdi <nour.mhamdi@ADDIXO.LOCAL>
2019-11-08 13:07:11 +0100

    test

--- src/main/java/com/Looyas/demo/constants/Paths.java
+++ src/main/java/com/Looyas/demo/constants/Paths.java
@@ -1,7 +1,7 @@
 package com.Looyas.demo.constants;
 
 public class Paths {
-    public static final String User ="/user";
+    public static final String USER ="http://192.168.10.193:8008/user/getUsers";
     public static final String Auth ="/auth";
 
 
--- src/test/java/com/Looyas/demo/unitTest/UserTest/UserControllerTest.java
+++ src/test/java/com/Looyas/demo/unitTest/UserTest/UserControllerTest.java
@@ -1,63 +1,78 @@
 //package com.Looyas.demo.unitTest.UserTest;
 //
-//import com.Looyas.demo.controllers.AuthController;
 //import com.Looyas.demo.controllers.UserController;
-//import com.Looyas.demo.messages.requests.SignUpForm;
 //import com.Looyas.demo.models.User;
+//import com.Looyas.demo.repositories.UserRepository;
 //import org.junit.Before;
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 //import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.mock.mockito.MockBean;
 //import org.springframework.http.MediaType;
 //import org.springframework.http.ResponseEntity;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.test.context.support.WithMockUser;
 //import org.springframework.test.context.junit4.SpringRunner;
 //import org.springframework.test.web.servlet.MockMvc;
 //import org.springframework.test.web.servlet.MvcResult;
 //import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 //
 //import java.net.URI;
+//import java.util.ArrayList;
+//import java.util.Collection;
 //import java.util.List;
 //
-//import static com.Looyas.demo.constants.Paths.VERSION;
+//import static com.Looyas.demo.constants.Paths.USER;
+//
 //import static java.util.Collections.singletonList;
 //import static junit.framework.TestCase.assertTrue;
 //import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 //import static org.hamcrest.core.Is.is;
 //import static org.junit.Assert.assertEquals;
 //import static org.mockito.BDDMockito.given;
-//import static org.mockito.BDDMockito.willReturn;
 //import static org.springframework.http.MediaType.APPLICATION_JSON;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 //
-//
-//@RunWith(SpringRunner.class)
+//@SpringBootTest
+////@RunWith(SpringRunner.class) //is used to provide a bridge between Spring Boot test features and JUnit
 //@WebMvcTest(UserController.class)
+//@AutoConfigureMockMvc
 //public class UserControllerTest {
 //    @Autowired
-//    private MockMvc            mvc;
+//    private MockMvc mvc;
 //
 //    @MockBean
-//    private AuthController authContrller;
-//    @MockBean
-//    SignUpForm signUpRequest;
+//    private UserController userController;
+//
+//    UserRepository userRepository;
 //    @Test
-//    public void addUserTest() throws Exception {
-//        signUpRequest user = new SignUpForm();
-//        signUpRequest.setEmail("nour.mhamdi@looyas.com");
-//        signUpRequest.setUserName("myusername");
-//        signUpRequest.setEmail("hhhhh");
+//    public void getAllUsersTest() throws Exception {
 //
-//        given(authContrller.registerUser(signUpRequest)).willReturn(() );
-//        URI auth;
-//        mvc.perform(get(auth  )                // .with(employee("blaze").password("Q1w2e3r4"))
+//        User user = new User();
+//        user.setUserId("xxx");
+//        user.setEmail("nour.mhamdi@looyas.com");
+//        user.setUsername("myusername");
+//        user.setPassword("hhhhh");
+//        List<User> allUsers = new ArrayList<User>();
+//        allUsers.add(user);
+//        //given(userController.findAllUser()).willReturn((ResponseEntity<Iterable<User>>) allUsers);
+//        mvc.perform(get(USER)                // .with(employee("blaze").password("Q1w2e3r4"))
 //                .contentType(APPLICATION_JSON))
 //                .andExpect(status().isOk())
 //                .andExpect(jsonPath("$", hasSize(1)))
 //                .andExpect(jsonPath("$[0].userId", is(user.getUserId())));
 //    }
+//
+//
+//
 //
 //}
