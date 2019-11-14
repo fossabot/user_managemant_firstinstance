b7eb156c8e2b456c77b64ef97da53ecc21f85b15
Nour Mhamdi <nour.mhamdi@ADDIXO.LOCAL>
2019-11-14 10:28:53 +0100

    test to pull

---
+++ src/test/java/com/Looyas/demo/unitTest/EndPointsTest/EndPointsTest.java
@@ -0,0 +1,4 @@
+package com.looyas.demo.unitTest.EndPointsTest;
+
+public class EndPointsTest {
+}
--- src/test/java/com/Looyas/demo/unitTest/UserTest/UserServiceTest.java
+++ src/test/java/com/Looyas/demo/unitTest/UserTest/UserServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 ;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,6 +26,7 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class UserServiceTest {
     private MockMvc mvc;
+
     @Autowired
     private UserService userService;
     @Autowired
@@ -43,8 +46,9 @@ public class UserServiceTest {
         userRepository.save(user);
         List<User> result = (List<User>) userService.findAll();
         assertEquals(users.get(0).getUserId(), result.get(0).getUserId());
-
-
     }
+
+
+
 
 }
